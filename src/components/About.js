6d700d4bbeb498fe4696281a1dbec864fac87991
Brian Yoon <infobyoon@gmail.com>
2022-04-26 22:06:03 -0700

    add img to About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import personalc from '/Users/brianyoon/sei/projects/thinkaboutmovies.github.io/src/personalc.jpeg';
 
 function About(props) {
     return (
-        <div id='About'>
-            About Component
-        </div>
-    );
+			<div id='About'>
+				<img src={personalc} alt='' />
+                <h2>hello ther is a </h2>
+			</div>
+		);
 }
 
 export default About;
