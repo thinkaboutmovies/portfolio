77cdb83e8d9bd559895ff2e4515d051f81679350
Brian Yoon <infobyoon@gmail.com>
2022-04-26 19:33:12 -0700

    Anchors for components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 function App() {
   return (
     <div >
+      <nav>
+        <ul class='main'>
+          <li> <a href="#Home">Home</a> </li>
+          <li> <a href="#About">About Me</a> </li>
+          <li> <a href="#Skills">Skills</a> </li>
+          <li> <a href="#Contact">Contact Info</a> </li>
+          <li> <a href="#Projects">Projects</a> </li>
+          <li> <a href="#Resume">Resume</a> </li>
+        </ul>
+      </nav>
+      <main>
 
+      </main>
     </div>
   );
 }
