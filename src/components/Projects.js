102b6ec27cdbd6a1e1c7849284e43a1dae84eeb1
Brian Yoon <infobyoon@gmail.com>
2022-04-28 02:23:01 -0700

    add rotating art gallery card to Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import RotatingArtGalleryScreenShot from '../images/RotatingArtGalleryScreenShot.png';
 
 function Projects(props) {
     return (
         <div id='Projects'>
-            Projects Component
+            <h2>Projects</h2>
+            <div id='Projects-Card-List'>
+                <div id='Rotating-Art-Gallery-Card'>
+                    <img src={RotatingArtGalleryScreenShot} />
+                    <h3>Rotating Art Gallery</h3>
+                    <p>Dynamically view your NFTs as a rotating artshow</p>
+                </div>
+            </div>
         </div>
     );
 }
