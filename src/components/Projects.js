b144e46a616a70670ac81f601824f1d47d71b8e3
Brian Yoon <infobyoon@gmail.com>
2022-04-28 02:57:47 -0700

    basic structure for Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,22 +10,22 @@ function Projects(props) {
             <h2>Projects</h2>
             <div id='Projects-Card-List'>
                 <div id='Rotating-Art-Gallery-Card'>
-                    <img src={RotatingArtGalleryScreenShot} />
+                    <img src={RotatingArtGalleryScreenShot} alt=''/>
                     <h3>Rotating Art Gallery</h3>
                     <p>Dynamically view your NFTs as a rotating artshow.</p>
                 </div>
                 <div id='Pomodoro-App-Card'>
-                    <img src={PomodoroAppScreenShot} />
+                    <img src={PomodoroAppScreenShot} alt=''/>
                     <h3>Pomodoro App</h3>
                     <p>Set timers and track study and break sessions using time intervals. </p>
                 </div>
                 <div id='Coin-Pouch-Card'>
-                    <img src={CoinPouchScreenShot} />
+                    <img src={CoinPouchScreenShot} alt=''/>
                     <h3>Coin Pouch</h3>
                     <p>How much does ETH does this NFT person have in their wallet?</p>
                 </div>
                 <div id='Dinosaur-Pet-Card'>
-                    <img src={DinosaurPetsScreenShot} />
+                    <img src={DinosaurPetsScreenShot} alt=''/>
                     <h3>Dinosaur Pet</h3>
                     <p>Raise your own dinosaur to have a happy life.</p>
                 </div>
