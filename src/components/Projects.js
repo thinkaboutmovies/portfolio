a0f4261d870e10bed5634e7aad7dd22da6eb44b6
Brian Yoon <infobyoon@gmail.com>
2022-06-10 11:45:56 -0700

    anchor tags for project images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,7 +10,12 @@ function Projects(props) {
 				<h2>Projects</h2>
 				<section id='Projects-Card-List'>
 					<article id='Rotating-Art-Gallery-Card'>
-						<img src={RotatingArtGalleryScreenShot} alt='' />
+						<a
+							href='https://github.com/thinkaboutmovies/rotating-art-gallery'
+							target='_blank'
+							rel='noreferrer'>
+							<img src={RotatingArtGalleryScreenShot} alt='' />
+						</a>
 						<h3>Rotating Art Gallery</h3>
 						<p>Dynamically view your NFTs as a rotating artshow.</p>
 						<h4>Tech Used</h4>
@@ -20,7 +25,12 @@ function Projects(props) {
 						</p>
 					</article>
 					<article id='Pomodoro-App-Card'>
-						<img src={PomodoroAppScreenShot} alt='' />
+						<a
+							href='https://github.com/Team-Clippers/Pomodoro-Frontend'
+							target='_blank'
+							rel='noreferrer'>
+							<img src={PomodoroAppScreenShot} alt='' />
+						</a>
 						<h3>Pomodoro App</h3>
 						<p>
 							Set timers and track study and break sessions using time
@@ -30,14 +40,24 @@ function Projects(props) {
 						<p>React.js, Flexbox, Mongoose, Express, Node.js, and Cors</p>
 					</article>
 					<article id='Coin-Pouch-Card'>
-						<img src={CoinPouchScreenShot} alt='' />
+						<a
+							href='https://github.com/thinkaboutmovies/coin-pouch'
+							target='_blank'
+							rel='noreferrer'>
+							<img src={CoinPouchScreenShot} alt='' />
+						</a>
 						<h3>Coin Pouch</h3>
 						<p>How much does ETH does this NFT person have in their wallet?</p>
 						<h4>Tech Used</h4>
 						<p> React.js, Etherscan API, and CSS Grids</p>
 					</article>
 					<article id='Dinosaur-Pet-Card'>
-						<img src={DinosaurPetsScreenShot} alt='' />
+						<a
+							href='https://github.com/thinkaboutmovies/dinosaur-pets'
+							target='_blank'
+							rel='noreferrer'>
+							<img src={DinosaurPetsScreenShot} alt='' />
+						</a>
 						<h3>Dinosaur Pet</h3>
 						<p>Raise your own dinosaur to have a happy life.</p>
 						<h4>Tech Used</h4>
