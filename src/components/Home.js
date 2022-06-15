286ff0c3ed05bff45a662b90eeeb9900c5ae935a
Brian Yoon <infobyoon@gmail.com>
2022-06-15 14:44:38 -0700

    prevent About component overlap into Hoome and Skills components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,20 +3,37 @@ import resume from '../images/Resume.pdf';
 
 function Home(props) {
     return (
-		<div id='Home' >
-            <ul class='nav'>
-                <li class='nav-element'><a href="#Home" color="white" >Home</a></li>
-                <li class='nav-element'> <a href="#About">About Me</a> </li>
-                <li class='nav-element'><a href="#Skills">Skills</a></li>
-                <li class='nav-element'><a href="#Contact">Contact Info</a></li>
-                <li class='nav-element'><a href="#Projects">Projects</a></li>
-                <li class='nav-element'><a href={resume}>Resume PDF</a></li>
-            </ul>
-				<div id='Home-Text'>
-					<p>Hello, I'm <span class='bold-text'>Brian Yoon</span> </p>
-					<p>and this is my Portfolio</p>
+			<div id='Home'>
+					<ul class='nav'>
+						<li class='nav-element'>
+							<a href='#Home' color='white'>
+								Home
+							</a>
+						</li>
+						<li class='nav-element'>
+							{' '}
+							<a href='#About'>About Me</a>{' '}
+						</li>
+						<li class='nav-element'>
+							<a href='#Skills'>Skills</a>
+						</li>
+						<li class='nav-element'>
+							<a href='#Contact'>Contact Info</a>
+						</li>
+						<li class='nav-element'>
+							<a href='#Projects'>Projects</a>
+						</li>
+						<li class='nav-element'>
+							<a href={resume}>Resume PDF</a>
+						</li>
+					</ul>
+					<div id='Home-Text'>
+						<p>
+							Hello, I'm <span class='bold-text'>Brian Yoon</span>{' '}
+						</p>
+						<p>and this is my Portfolio</p>
+					</div>
 				</div>
-			</div>
 		);
 }
 
